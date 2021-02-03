10d01912bdf92e89457fdfff709145580fe15bd0
ccsounds <52943230+ccsounds@users.noreply.github.com>
2021-02-03 18:55:31 -0500

    added a launch config

--- basic.js
+++ basic.js
@@ -1,5 +1,10 @@
 const os = require('os');
+const chance = require('chance');
+
+const mychance = new chance();
 
 console.log("hi");
 
 console.log("it looks like you're running a " + os.platform() + " machine.");
+
+console.log("your new fake name is " + mychance.name());
